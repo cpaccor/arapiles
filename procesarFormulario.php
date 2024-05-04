c23e34eac24855b48e403928324e01a368328490
cpaccor <cristian_pac@live.com>
2024-05-04 23:35:23 +0200

    se ha actualizado el procesamiento del formulario, añadido email

--- procesarFormulario.php
+++ procesarFormulario.php
@@ -1,29 +1,16 @@
 <?php
 require_once 'conexion.php';
-
-/*try {
-    // Preparar la consulta SQL para insertar un nuevo equipo
-    $insertarEquipo = "INSERT INTO equipos(nombreEquipo) VALUES ('$nombreEquipo')"; 
-    $resultado = $bdd->query($insertarEquipo);   
-    //comprobamos los errores
-    if ($resultado){
-        echo "Equipo insertado correctamente. <br>";
-        echo "filas insertadas: ".$resultado->rowcount()."<br>";        
-    }  else print_r($bdd->errorInfo()); 
-    echo "Dorsal del equipo:". $bdd->lastInsertId()."<br>";  
-    
-} catch (PDOException $ex) {
-    echo "Error al insertar equipo: " . $ex->getMessage();
-}*/
-
 // Verificamos si se ha enviado el formulario mediante un POST
 if ($_SERVER[ "REQUEST_METHOD" ] == "POST") {
+    //inicializamos la variable mensaje que almacenaremos
+    $mensaje = "";
+    $mensajeError = "";
 
     /*** Bloque para introducir nombre equipo*****/
     // asignamos el nombre del equipo recibido en una variable
     $nombreEquipo = $_POST[ "nombreEquipo" ];
-    
-    
+
+
     try {//reliazamos captura de errores
         //preparamos una consulta para comprobar si el nombre del equipo ya existe en la base de datos
         $consulta = $bdd->prepare("SELECT COUNT(*) AS coincidencia FROM equipos WHERE nombreEquipo = :nombreEquipo");
@@ -34,101 +21,78 @@ if ($_SERVER[ "REQUEST_METHOD" ] == "POST") {
         //si fila es mayor a 0 existira algun nombre en la base de datos
         if ($fila[ 'coincidencia' ] > 0) {
             // Si el nombre de equipo ya existe almacenamos el error en la variable.
-            $mensaje = "Ya existe un equipo con ese nombre. Por favor, elige otro nombre.";
+            $mensajeError = "Ya existe un equipo con ese nombre. Por favor, elige otro nombre.";
             //redirigimos al formularioo y almacenamos un mensaje
-            header("Location: inscripcion.html?mensaje=" . urlencode($mensaje));
+            header("Location: inscripcion.html?mensajeError=" . urlencode($mensajeError));
             exit();
 
         } else {
-            // Si el nombre de equipo no existe, proceder con el procesamiento del formulario
-            // Aquí iría el código para insertar los datos en la base de datos
+            // si no existiese el nombre del equipo procesaremos el formulario
+            // preparamos mla consulta para insertar los datos en la base de datos
             $insertarEquipo = $bdd->prepare("INSERT INTO equipos(nombreEquipo) VALUES (:nombreEquipo)");
+            //vinculamos cada marcador de posicion con su variable correspondiente
             $insertarEquipo->bindParam(":nombreEquipo", $nombreEquipo, PDO::PARAM_STR);
+           //ejecutamos la consulta y la almacenamos en una variable
             $resultado = $insertarEquipo->execute();
+            //En caso de si o no se haya ejecutado la consulta alamcenaremos un mensaje, segun corresponda.
             if ($resultado) {
-                $mensaje = "Equipo introducido correctamente con dorsal nº: " . $bdd->lastInsertId();
+                $mensaje = "Equipo iscrito correctamente con dorsal nº: " . $bdd->lastInsertId() . "<br>";
             } else {
-                $mensaje = "Equipo no se ha podido inscribir correctamente";
+                $mensajeError = $mensajeError +"Equipo no se ha podido inscribir correctamente";
             }
             //redirigimos al formularioo y almacenamos un mensaje
-            
+
         }
     } catch (PDOException $incidencia) {
         echo "Error en la inserción de equipo " . $incidencia->getMessage();
     }
 
     $idEquipo = $bdd->lastInsertId();
-    
-    try {
-        for ($i=1; $i <=4 ; $i++) {             
-            $nombre = $_POST[ "nombre_".$i];
-            $apellido1 = $_POST[ "apellido1_".$i];
-            $apellido2 = $_POST[ "apellido2_".$i];
-            $dni = $_POST[ "dni_".$i];
-            $sexo = $_POST["sexo_".$i];
-            $fechaNacimiento = $_POST["fechaNacimiento_".$i];
-            $fcse = $_POST["fcse_".$i];
-            $matricula =$_POST["matricula_".$i];
+
+    try {//capturamos los posibles errores
+        //creamos un bucle que se repetira 4 veces para ir recorriendo los datos enciados pro el formulario
+        for ($i = 1; $i <= 4; $i++) {
+            //declaramos las variables que iran almacenando los datos más el correspondiente valor de  "i"            
+            $nombre = $_POST["nombre_" . $i];
+            $apellido1 = $_POST["apellido1_" . $i];
+            $apellido2 = $_POST["apellido2_" . $i];
+            $dni = $_POST["dni_" . $i];
+            $sexo = $_POST["sexo_" . $i];
+            $fechaNacimiento = $_POST["fechaNacimiento_" . $i];
+            $fcse = $_POST["fcse_" . $i];
+            $matricula = $_POST["matricula_" . $i];
+            $email = $_POST["email_" . $i];
+            //preparamos la consulta que insertará los datos a la vace de datos
             $insertarParticipante = $bdd->prepare(
-                "INSERT INTO participantes (idEquipo, nombre, apellido1, apellido2, dni, sexo, fechaNacimiento, fcse, matricula) 
-                VALUES (:idEquipo, :nombre, :apellido1, :apellido2, :dni, :sexo, :fechaNacimiento, :fcse, :matricula)");
-        //vinculamos cada marcador de posición con la variable correspondiente
-        $insertarParticipante->bindParam(":idEquipo", $idEquipo, PDO::PARAM_INT);
-        $insertarParticipante->bindParam(":nombre", $nombre, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":apellido1", $apellido1, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":apellido2", $apellido2, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":dni", $dni, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":sexo", $sexo, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":fechaNacimiento", $fechaNacimiento, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":fcse", $fcse, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":matricula", $matricula, PDO::PARAM_STR);
+                
+                "INSERT INTO participantes (idEquipo, nombre,  apellido1, apellido2, dni, sexo, fechaNacimiento, fcse, matricula, email) 
+                VALUES (:idEquipo, :nombre, :apellido1, :apellido2, :dni, :sexo, :fechaNacimiento, :fcse, :matricula, :email)"
+                );
+            //vinculamos cada marcador de posición con la variable correspondiente
+            $insertarParticipante->bindParam(":idEquipo", $idEquipo, PDO::PARAM_INT);
+            $insertarParticipante->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":apellido1", $apellido1, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":apellido2", $apellido2, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":dni", $dni, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":sexo", $sexo, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":fechaNacimiento", $fechaNacimiento, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":fcse", $fcse, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":matricula", $matricula, PDO::PARAM_STR);
+            $insertarParticipante->bindParam(":email", $email, PDO::PARAM_STR);
 
-        $insertarParticipante->execute();
+            //ejecutamos la consulta y la almacenamos en una variable que posteriormente utilizaremos para comprobar si ha hab ido algun error
+            $completado = $insertarParticipante->execute();
+            if (!$completado) {
+                $mensajeError = $mensajeError . "Error al insertar participantes";
+                //redirigimos al formularioo y almacenamos un mensaje
+                header("Location: inscripcion.html?mensajeError=" . urlencode($mensaje));
+                exit();
+            }
         }
-        
-        /*$insertarParticipante = $bdd->prepare("INSERT INTO participantes (id_equipo, nombre, apellido1, apellido2, dni, sexo, fechaNacimiento, fcse, matricula) VALUES (NULL, $nombre, $apellido1, $apellido2, $dni, $sexo, $fechaNacimiento, $fcse, $matricula)");
-        $nombre = $_POST[ "nombre_1" ];
-    $apellido1 = $_POST[ "apellido1_1" ];
-    $apellido2 = $_POST[ "apellido2_1" ];
-    $dni = $_POST[ "dni_1" ];
-    $sexo = $_POST[ "sexo_1" ];
-    $fechaNacimiento = $_POST[ "fecha_nacimiento_1" ];
-    $fcse = isset($_POST[ "fcse_1" ]) ? $_POST[ "fcse_1" ] : "No";
-    $matriculaVehiculo = isset($_POST[ "matricula_1" ]) ? $_POST[ "matricula_1" ] : "";
-        /*$insertarParticipante->bindParam(":nombre", $nombre, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":apellido1", $apellido1, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":apellido2", $apellido2, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":dni", $dni, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":sexo", $sexo, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":fechaNacimiento", $fechaNacimiento, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":fcse", $fcse, PDO::PARAM_STR);
-        $insertarParticipante->bindParam(":matricula", $matricula, PDO::PARAM_STR);*/
-
-        //$insertarParticipante->execute();
-
-    } catch (\Throwable $th) {
-        //throw $th;
+    } catch (PDOException $incidencia) {
+        echo "Error en la inserción de los participantes " . $incidencia->getMessage();
     }
-
-
-    /*try {//reliazamos captura de errores
-    // Preparar la consulta SQL para insertar un nuevo equipo    
-    $insertarEquipo = "INSERT INTO equipos(nombreEquipo) VALUES ('$nombreEquipo')";
-    //realizamos la consulta a la base de dato 
-    $resultado = $bdd->query($insertarEquipo);   
-
-    //comprobamos los errores
-    if ($resultado){
-        echo "Equipo insertado correctamente. <br>";
-        echo "filas insertadas: ".$resultado->rowcount()."<br>";        
-    }  else print_r($bdd->errorInfo()); 
-    echo "Dorsal del equipo:". $bdd->lastInsertId()."<br>"; 
-    
-    } catch (PDOException $ex) {
-    echo "Error al insertar equipo: " . $ex->getMessage();
-    }*/
-
-
+    //redirigimos al formularioo y almacenamos un mensaje
     header("Location: inscripcion.html?mensaje=" . urlencode($mensaje));
     exit();
 }
